8c2f351c5f61feb2cef7f11155612f57c652ae51
kaye <472655949@qq.com>
2019-02-14 16:24:01 +0800

    fix

--- lockdemo/src/controller/CallableFutureDemo.java
+++ lockdemo/src/controller/CallableFutureDemo.java
@@ -20,11 +20,17 @@ public class CallableFutureDemo {
         });
         Future future = executorService.submit(futureTask);
 
+        //返回null表示线程执行完成
         System.err.println(future.get());
+        //线程是否执行完成
         System.err.println(future.isDone());
+        //线程是被被取消
         System.err.println(future.isCancelled());
+        //返回线程的值
         System.err.println(futureTask.get());
+        //线程释放执行完成
         System.err.println(futureTask.isDone());
+        //线程是否被取消
         System.err.println(futureTask.isCancelled());
 
         executorService.shutdown();
