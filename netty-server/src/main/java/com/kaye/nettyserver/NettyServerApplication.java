906a9fbfa722e785c06bf7a5dd6ed2596d4409f7
kaye <472655949@qq.com>
2019-02-20 11:37:52 +0800

    搭建一个nettyserver服务

---
+++ netty-server/src/main/java/com/kaye/nettyserver/NettyServerApplication.java
@@ -0,0 +1,17 @@
+package com.kaye.nettyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 演示一个在项目中netty的使用
+ * @author yk
+ */
+@SpringBootApplication
+public class NettyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyServerApplication.class, args);
+    }
+
+}
