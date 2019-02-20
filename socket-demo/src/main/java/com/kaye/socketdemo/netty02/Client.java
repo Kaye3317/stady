fedca9d12891fc1b7dab1ee1f0ad9aa67abeb37f
kaye <472655949@qq.com>
2019-02-20 14:45:00 +0800

    添加定长方式解决拆包粘包

--- socket-demo/src/main/java/com/kaye/socketdemo/netty02/Client.java
+++ socket-demo/src/main/java/com/kaye/socketdemo/netty02/Client.java
@@ -10,6 +10,7 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
@@ -30,8 +31,18 @@ public class Client {
                     .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            /*//1.start指定分割符的方式解决拆包粘包
+                            // 当遇到 $_时就当成一次数据的请求 要求必须转换成buffer类型的
                             ByteBuf buf = Unpooled.copiedBuffer("$_".getBytes());
-                            socketChannel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, buf));
+                            socketChannel.pipeline().addLast(new DelimiterBasedFrameDecoder(
+                                    //这个参数是指定特殊分割符的最大字节
+                                    1024,
+                                    //这个参数是指定特殊分割符
+                                    buf));
+                            //1.end*/
+                            //2.使用消息定长来解决拆包粘包问题 如果长度不够需要用空格填充否则不会发送
+                            //建议使用特殊字符的方式来拆包粘包
+                            socketChannel.pipeline().addLast(new FixedLengthFrameDecoder(10));
                             //指定字符串解码格式，这里指定的是字符串类型的解码
                             //设置了这个在handler里就不用把msg转换成ByteBuf再转换成String了
                             //这样设置handler里直接收到的就是String
@@ -44,10 +55,10 @@ public class Client {
                     });
             ChannelFuture f = b.connect("127.0.0.1", 9998).sync();
             //因为设置了编码所以可以直接写字符串
-            f.channel().writeAndFlush("client--1$_");
-            f.channel().writeAndFlush("client--2$_");
-            f.channel().writeAndFlush("client--3$_");
-            f.channel().writeAndFlush("client--4$_");
+            f.channel().writeAndFlush("client-1$_");
+            f.channel().writeAndFlush("client-2$_");
+            f.channel().writeAndFlush("client-3$_");
+            f.channel().writeAndFlush("client-4$_");
             f.channel().closeFuture().sync();
         } finally {
             worker.shutdownGracefully();
